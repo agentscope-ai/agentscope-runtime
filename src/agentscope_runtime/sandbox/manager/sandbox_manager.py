d87b53ded0ab691bd897599fa07832ae7fffaf7f
陈子谦 <eric.czq@alibaba-inc.com>
2025-08-28 23:24:12 +0800

    fixed sandbox_manager

--- src/agentscope_runtime/sandbox/manager/sandbox_manager.py
+++ src/agentscope_runtime/sandbox/manager/sandbox_manager.py
@@ -367,7 +367,6 @@ class SandboxManager:
         environment = {
             **(config.environment if config.environment else {}),
             **(environment if environment else {}),
-            
         }
 
         for key, value in environment.items():
@@ -600,7 +599,10 @@ class SandboxManager:
         enable_browser = "browser" in container_model.version
 
         # TODO: remake docker name
-        if "appworld" or 'bfcl' in container_model.version:
+        if (
+            "sandbox-appworld" in container_model.version
+            or "sandbox-appworld" in container_model.version
+        ):
             return TrainingSandboxClient(
                 base_url=f"http://localhost:{container_model.ports[0]}",
             ).__enter__()
