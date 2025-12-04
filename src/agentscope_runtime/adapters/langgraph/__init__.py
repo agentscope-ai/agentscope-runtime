08bce88fe74c31f23a507d55d7c156fc5e2ba771
zhiyong <wzy418013@alibaba-inc.com>
2025-12-04 19:09:53 +0800

    add langgraph todo

--- src/agentscope_runtime/adapters/langgraph/__init__.py
+++ src/agentscope_runtime/adapters/langgraph/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """LangGraph adapter for AgentScope runtime."""
 
+# todo Message(reasoning) Adapter
+# todo Memory Adapter
+# todo Sandbox Tools Adapter
 from .message import langgraph_msg_to_message, message_to_langgraph_msg
 
 __all__ = [
