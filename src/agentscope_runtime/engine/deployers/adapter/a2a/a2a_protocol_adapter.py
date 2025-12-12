536eeaae260c9e36c5491d61eb45efd9e7bd30e1
chenxinyu.cxy <chenxinyu.cxy@alibaba-inc.com>
2025-12-12 10:40:35 +0800

    fix pylint

--- src/agentscope_runtime/engine/deployers/adapter/a2a/a2a_protocol_adapter.py
+++ src/agentscope_runtime/engine/deployers/adapter/a2a/a2a_protocol_adapter.py
@@ -235,7 +235,7 @@ class A2AFastAPIDefaultAdapter(ProtocolAdapter):
     be overridden by users.
     """
 
-    def __init__(  # pylint: disable=R0917
+    def __init__(
         self,
         agent_name: str,
         agent_description: str,
