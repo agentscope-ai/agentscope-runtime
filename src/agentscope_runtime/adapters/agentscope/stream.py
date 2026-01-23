63deb3b918d2309e2e85ab44dcfe291a5a5fe2e6
raykkk <weirui.kwr@alibaba-inc.com>
2026-01-23 17:54:05 +0800

    support video block

--- src/agentscope_runtime/adapters/agentscope/stream.py
+++ src/agentscope_runtime/adapters/agentscope/stream.py
@@ -16,6 +16,7 @@ from ...engine.schemas.agent_schemas import (
     TextContent,
     ImageContent,
     AudioContent,
+    VideoContent,
     DataContent,
     McpCall,
     McpCallOutput,
@@ -511,6 +512,41 @@ async def adapt_agentscope_message_stream(
                                     {"format": media_type, "data": url},
                                 )
                             delta_content = AudioContent(
+                                delta=True,
+                                index=index,
+                                **kwargs,
+                            )
+                        elif element.get("type") == "video":
+                            kwargs = {}
+                            if (
+                                isinstance(element.get("source"), dict)
+                                and element.get("source", {}).get(
+                                    "type",
+                                )
+                                == "url"
+                            ):
+                                kwargs.update(
+                                    {"video_url": element.get("source")},
+                                )
+
+                            elif (
+                                isinstance(element.get("source"), dict)
+                                and element.get("source").get(
+                                    "type",
+                                )
+                                == "base64"
+                            ):
+                                media_type = element.get("source", {}).get(
+                                    "media_type",
+                                    "video/mp4",
+                                )
+                                base64_data = element.get("source", {}).get(
+                                    "data",
+                                    "",
+                                )
+                                url = f"data:{media_type};base64,{base64_data}"
+                                kwargs.update({"video_url": url})
+                            delta_content = VideoContent(
                                 delta=True,
                                 index=index,
                                 **kwargs,
