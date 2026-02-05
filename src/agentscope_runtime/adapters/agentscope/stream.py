7666d46e7a9530e63c0f9f03a33d359c5ec6c37a
raykkk <weirui.kwr@alibaba-inc.com>
2026-02-05 11:03:30 +0800

    support file

--- src/agentscope_runtime/adapters/agentscope/stream.py
+++ src/agentscope_runtime/adapters/agentscope/stream.py
@@ -19,6 +19,7 @@ from ...engine.schemas.agent_schemas import (
     AudioContent,
     VideoContent,
     DataContent,
+    FileContent,
     McpCall,
     McpCallOutput,
     FunctionCall,
@@ -598,6 +599,46 @@ async def adapt_agentscope_message_stream(
                                 url = f"data:{media_type};base64,{base64_data}"
                                 kwargs.update({"video_url": url})
                             delta_content = VideoContent(
+                                delta=True,
+                                index=index,
+                                **kwargs,
+                            )
+                        elif element.get("type") == "file":
+                            kwargs = {}
+                            if (
+                                isinstance(element.get("source"), dict)
+                                and element.get("source", {}).get(
+                                    "type",
+                                )
+                                == "url"
+                            ):
+                                kwargs.update(
+                                    {
+                                        "file_url": element.get(
+                                            "source",
+                                            {},
+                                        ).get("url"),
+                                    },
+                                )
+
+                            elif (
+                                isinstance(element.get("source"), dict)
+                                and element.get("source").get(
+                                    "type",
+                                )
+                                == "base64"
+                            ):
+                                media_type = element.get("source", {}).get(
+                                    "media_type",
+                                    "application/octet-stream",
+                                )
+                                base64_data = element.get("source", {}).get(
+                                    "data",
+                                    "",
+                                )
+                                url = f"data:{media_type};base64,{base64_data}"
+                                kwargs.update({"file_url": url})
+                            delta_content = FileContent(
                                 delta=True,
                                 index=index,
                                 **kwargs,
